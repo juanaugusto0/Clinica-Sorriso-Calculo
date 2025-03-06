e0bc38b52f905de25cefb120ca6958cee9f4dcd0
juanaugusto0 <jg14253@gmail.com>
2025-03-06 19:47:44 -0300

    commit

---
+++ src/main/java/com/sorrisodopovo/CalculoDentista/CalculoDentistaApplication.java
@@ -0,0 +1,13 @@
+package com.sorrisodopovo.CalculoDentista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoDentistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoDentistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sorrisodopovo/CalculoDentista/CalculoDentistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorrisodopovo.CalculoDentista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoDentistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
